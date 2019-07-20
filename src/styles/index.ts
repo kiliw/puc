b21ed4b8116f1a67dafdba3c0401fe6936a6ba18
kiliw <kilocheat@gmail.com>
2019-07-20 22:47:00 +0200

    First commit.

---
+++ src/styles/index.ts
@@ -0,0 +1,52 @@
+import { StyleSheet } from 'react-native'
+import { Colors } from 'react-native/Libraries/NewAppScreen'
+
+export const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  mainTitle: {
+    fontSize: 32,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionMainTitle: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+})
