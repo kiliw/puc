5590ce2fd378780f0fe61c02e2e1ee8f32990330
kiliw <kilocheat@gmail.com>
2019-08-22 15:01:52 +0200

    Add test for the reducer.

--- src/components/Counter/reducer.spec.ts
+++ src/components/Counter/reducer.spec.ts
@@ -1,20 +1,30 @@
 import * as actions from './actions'
-import { DayState, getTotalPullUps, totalPullUpsReducer } from './reducer'
-
-const MOCK_STATE: DayState = Object.freeze({
-  pullUps: 10,
-  totalPullUps: 20,
-})
-
-describe('Pull Up selectors', () => {
-  it('A user has done 20 Pull Ups in total', () => {
-    expect(getTotalPullUps(MOCK_STATE)).toEqual(20)
-  })
-})
+import { dayReducer, totalPullUpsReducer } from './reducer'
 
 describe('Pull Up reducers', () => {
   it('A user saves 10 Pull Ups', () => {
     expect(totalPullUpsReducer(0, actions.savePullUps(10))).toEqual(10)
     expect(totalPullUpsReducer(1, actions.savePullUps(10))).toEqual(11)
+  })
+})
+
+describe('Day reducers', () => {
+  it('A user has done 10 Pull Ups and saves them to the total amount for today', () => {
+    expect(dayReducer([], actions.savePullUps(10))).toEqual([
+      { totalPullUps: 10 },
+    ])
+    expect(
+      dayReducer(
+        [{ totalPullUps: 10 }, { totalPullUps: 10 }],
+        actions.savePullUps(10),
+      ),
+    ).toEqual([{ totalPullUps: 20 }, { totalPullUps: 10 }])
+  })
+  it('A user has done 10 Pull Ups and saves the day', () => {
+    expect(dayReducer([], actions.saveDay(10))).toEqual([{ totalPullUps: 10 }])
+    expect(dayReducer([{ totalPullUps: 10 }], actions.saveDay(11))).toEqual([
+      { totalPullUps: 10 },
+      { totalPullUps: 11 },
+    ])
   })
 })
