786771941b9184604b74a1a8538364d05f433dcd
kiliw <kilocheat@gmail.com>
2019-08-24 16:33:05 +0200

    Reorganize the design and implement more nativebase components.

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,30 +1,18 @@
+import { Container } from 'native-base'
 import React from 'react'
-import { ScrollView, StyleSheet, View } from 'react-native'
+import { SafeAreaView } from 'react-native'
 import { Home } from '../components/Home'
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <ScrollView
-        style={styles.container}
-        contentContainerStyle={styles.contentContainer}
-      >
+    <Container>
+      <SafeAreaView>
         <Home />
-      </ScrollView>
-    </View>
+      </SafeAreaView>
+    </Container>
   )
 }
 
 HomeScreen.navigationOptions = {
   header: null,
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#fff',
-    flex: 1,
-  },
-  contentContainer: {
-    paddingTop: 30,
-  },
-})
