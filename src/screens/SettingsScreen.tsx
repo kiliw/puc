0e527d482f32aef82d908c6646fd6583ad4e3aff
kiliw <kilocheat@gmail.com>
2019-10-03 21:51:49 +0200

    Add feature to delete the whole history of pull ups to the settings screen.

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -1,12 +1,20 @@
 import { ExpoConfigView } from '@expo/samples'
+import { Container } from 'native-base'
 import React from 'react'
+import { View } from 'react-native'
+import { Settings } from '../components/Settings'
 
 export default function SettingsScreen() {
   /**
    * Go ahead and delete ExpoConfigView and replace it with your content;
    * we just wanted to give you a quick view of your config.
    */
-  return <ExpoConfigView />
+  return (
+    <Container>
+      <ExpoConfigView />
+      <Settings />
+    </Container>
+  )
 }
 
 SettingsScreen.navigationOptions = {
