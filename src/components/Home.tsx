10b4b05d37179eb2c8900b2786732d350e62e01c
kiliw <kilocheat@gmail.com>
2019-10-24 08:11:38 +0200

    Add chart kit.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,35 @@
 import React, { FunctionComponent } from 'react'
 import { Text, View } from 'react-native'
+import { Dimensions } from 'react-native'
+import { LineChart } from 'react-native-chart-kit'
 import { TouchableHighlight } from 'react-native-gesture-handler'
 import { useSelector } from 'react-redux'
 import * as selectors from '../selectors'
 import { styles } from '../styles'
 import { HomeHeadline } from './Header'
+
+const screenWidth = Dimensions.get('window').width
+
+const chartConfig = {
+  backgroundGradientFrom: '#1E2923',
+  backgroundGradientFromOpacity: 0,
+  backgroundGradientTo: '#08130D',
+  backgroundGradientToOpacity: 0.5,
+  barPercentage: 0.5,
+  color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+  strokeWidth: 2, // optional, default 3
+}
+
+const data = {
+  datasets: [
+    {
+      color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})`, // optional
+      data: [20, 45, 28, 80, 99, 43],
+      strokeWidth: 2, // optional
+    },
+  ],
+  labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+}
 
 export const Home: FunctionComponent = (
   props, // TODO Add typings for the navigation
@@ -31,6 +56,14 @@ export const Home: FunctionComponent = (
           <Text style={{ fontSize: 60, paddingTop: 30, textAlign: 'center' }}>
             {overAllPullUps}
           </Text>
+          <LineChart
+            data={data}
+            width={screenWidth}
+            height={256}
+            verticalLabelRotation={30}
+            chartConfig={chartConfig}
+            bezier
+          />
         </View>
       </View>
     </View>
